3cd147e9d2312da7bd0a9d08afe0ef8a9b1ff8d8
Dinithi Rajapaksha <153760230+DinithiTR@users.noreply.github.com>
2024-10-14 19:46:50 +0530

    MedicalRecords and Patients

--- src/pages/staff/MedicalRecords.jsx
+++ src/pages/staff/MedicalRecords.jsx
@@ -1,8 +1,15 @@
+// frontend/src/pages/staff/MedicalRecords.jsx
+
 import React from 'react'
+import BackNavigation from '../../components/pagecomponents/BackNavigation'
+import { StaffMedicalRecordsTable } from '../../components/pagecomponents/staff/MedicalRecordsTable'
 
 const MedicalRecords = () => {
   return (
-    <div>MedicalRecords</div>
+    <div className='w-full'>
+      <BackNavigation label="Medical Records" btn={true} btnTitle={"Add a Record"} btnStyle={'bg-green'}/>
+      <StaffMedicalRecordsTable />
+    </div>
   )
 }
 
